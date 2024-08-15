ab0ce5918f3df48f03bb8aefe08544347651bb90
Jon Howard <johnny.rtn@gmail.com>
2024-08-15 16:20:11 -0230

    progress

--- src/app/(customerFacing)/products/[id]/purchase/page.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -1,6 +1,7 @@
 import db from "@/components/db/db"
 import { notFound } from "next/navigation"
 import Stripe from "stripe"
+import { CheckoutForm } from "./_components/CheckoutForm"
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 
@@ -22,5 +23,10 @@ export default async function PurchasePage({
     throw Error("Stripe failed to create payment intent")
   }
 
-  return <h1>Hi!</h1>
+  return (
+    <CheckoutForm
+      product={product}
+      clientSecret={paymentIntent.client_secret}
+    />
+  )
 }
