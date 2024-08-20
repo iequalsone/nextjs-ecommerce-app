6fc32fddf2f904a32b7af884ed4faaafba723066
Jon Howard <johnny.rtn@gmail.com>
2024-08-20 04:41:13 -0230

    add stripe checkout logic

--- src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -1,17 +1,30 @@
 "use client"
 
+import { userOrderExists } from "@/app/actions/orders"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 import { formatCurrency } from "@/lib/formatters"
 import {
   Elements,
+  LinkAuthenticationElement,
   PaymentElement,
   useElements,
   useStripe,
 } from "@stripe/react-stripe-js"
 import { loadStripe } from "@stripe/stripe-js"
 import Image from "next/image"
+import { FormEvent, useState } from "react"
 
 type CheckoutFormProps = {
   product: {
+    id: string
     imagePath: string
     name: string
     priceInCents: number
@@ -47,15 +60,88 @@ export function CheckoutForm({ product, clientSecret }: CheckoutFormProps) {
         </div>
       </div>
       <Elements stripe={stripePromise} options={{ clientSecret }}>
-        <Form />
+        <Form priceInCents={product.priceInCents} productId={product.id} />
       </Elements>
     </div>
   )
 }
 
-function Form() {
+function Form({
+  priceInCents,
+  productId,
+}: {
+  priceInCents: number
+  productId: string
+}) {
   const stripe = useStripe()
   const elements = useElements()
+  const [isLoading, setIsLoading] = useState(false)
+  const [errorMessage, setErrorMessage] = useState<string>()
+  const [email, setEmail] = useState<string>()
 
-  return <PaymentElement />
+  const disabled = !stripe || !elements || isLoading || !email
+
+  async function handleSubmit(e: FormEvent) {
+    e.preventDefault()
+
+    if (disabled) return
+
+    setIsLoading(true)
+
+    const orderExists = await userOrderExists(email, productId)
+
+    if (orderExists) {
+      setErrorMessage(
+        "You have already purchased this product. Try downloading it from the My Orders page."
+      )
+      setIsLoading(false)
+      return
+    }
+
+    stripe
+      .confirmPayment({
+        elements,
+        confirmParams: {
+          return_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/stripe/purchase-success`,
+        },
+      })
+      .then(({ error }) => {
+        if (error.type === "card_error" || error.type === "validation_error") {
+          setErrorMessage(error.message)
+        } else {
+          setErrorMessage("An unknown error occurred")
+        }
+      })
+      .finally(() => setIsLoading(false))
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Checkout</CardTitle>
+          {errorMessage && (
+            <CardDescription className="text-destructive">
+              {errorMessage}
+            </CardDescription>
+          )}
+        </CardHeader>
+        <CardContent>
+          <PaymentElement />
+          <div className="mt-4">
+            <LinkAuthenticationElement
+              onChange={(e) => setEmail(e.value.email)}
+            />
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full" size="lg" disabled={disabled}>
+            {isLoading
+              ? "Purchasing..."
+              : `Purchase - ${formatCurrency(priceInCents / 100)}`}
+          </Button>
+        </CardFooter>
+      </Card>
+    </form>
+  )
 }
